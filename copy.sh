fb70ea0d92edced1bc90c7f5dacdfff8dff05f20
johnbonda <john.bonda7@gmail.com>
2019-04-30 20:20:32 +0530

    Added a script file to install

---
+++ copy.sh
@@ -0,0 +1,7 @@
+mkdir ~/.config
+ln -s ~/dotfiles/.config/i3 ~/.config/
+ln -s ~/dotfiles/.vim ~/
+ln -s ~/dotfiles/.bashrc ~/
+ln -s ~/dotfiles/.Xmodmap ~/
+ln -s ~/dotfiles/.xsession ~/
+echo "All done"
